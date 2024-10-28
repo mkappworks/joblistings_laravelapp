9cf3b3c3ebb6d980e84fcf9a6e9d9b5b7f4be6aa
Malith Kuruppu <info.mkappworks@gmail.com>
2024-10-28 06:54:34 +0100

    Add job and jobs views and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::view("/", "home");
 
 Route::view("/contact", "contact");
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    if (!$job) {
+        abort(404);
+    }
+
+    return view('job', ['job' => $job]);
+});
