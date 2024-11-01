ec058735c97ba40dfe3927d8bef855ac7e124f24
Malith Kuruppu <info.mkappworks@gmail.com>
2024-11-02 00:09:18 +0100

    Implement JobController and refactor job routes to use resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,71 +1,23 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view("/", "home");
-
 Route::view("/contact", "contact");
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(7);
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/jobs/{job}', function (Job $job) {
-    return view('jobs.show', ['job' => $job]);
-});
-
-
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    return view('jobs.edit', [
-        'job' => $job
-    ]);
-});
-
-Route::patch('/jobs/{job}', function (Job $job) {
-    //TODO:: authorize
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-
-Route::delete('/jobs/{job}', function (Job $job) {
-    //TODO:: authorize
-
-    $job->delete();
-
-    return redirect('/jobs');
-});
+Route::resource('jobs', JobController::class, [
+    //use one or the other
+    // 'only' => ['index'],
+    // 'except' => ['edit']
+]);
